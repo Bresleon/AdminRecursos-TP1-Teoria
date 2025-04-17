9253957ea1bb0b30447c32c0f8fee60e58215361
Leon Breslauer <leonbreslauer.tuc@gmail.com>
2025-04-17 13:11:39 -0300

    Agregar archivos de proyecto.

---
+++ AdminRecursos-TP1-Teoria/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AdminRecursos_TP1_Teoria.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
